6287170340971d417085befb5cae492d3c9f63d3
PutkisDude <putkis@gmail.com>
2022-11-10 21:21:26 +0200

    change start allocation to database procedure

--- routes/allocation.js
+++ routes/allocation.js
@@ -124,36 +124,12 @@ allocation.post("/reset", async (req, res) => {
 // Allokointilaskennan aloitus - KESKEN!
 allocation.post("/start", async (req, res) => {
     const allocRound = req.body.allocRound;
-    console.time("start");
     if(!allocRound) {
         return validationErrorHandler(res, "Missing required parameter - allocation start");
     }
-
-    await allocationService.getPriorityOrder(allocRound) // "laskee" prioriteettijärjestyksen
-    .then(async data => { // merkitsee prioriteettinumerot allocSubjecteihin
-        await Promise.all(data.map((subject, index) => allocationService.updateAllocSubjectPriority(subject.subjectId, subject.allocRound, index +1)));
-        return data;
-    })
-    .then(async data => { // Etsii opetukselle sopivat tilat ja tallentaa ne allocSubjectSuitableSpace tauluun
-        await Promise.all(data.map((subject) => {
-             return allocationService.findRoomsForSubject(allocRound, subject.subjectId)
-            })
-        )
-        return data;
-    })
-    .then(async data => {
-        return await Promise.all(data.map(async (subject) => { // Asettaa montako varustetta puuttuu tilasta. PITÄÄ TEHDÄ PAREMPI!
-                const suitableRooms = await allocationService.getSuitableRoomsForSubject(allocRound, subject.subjectId);
-                await suitableRooms.map(async (room) => {
-                    const missingItemAmount = await allocationService.getMissingItemAmount(subject.subjectId, room.spaceId);
-                    await allocationService.setMissingItemAmount(subject.subjectId, room.spaceId, missingItemAmount[0].missingItems)
-                    .catch(e => console.log(e));
-                })
-            })
-        )
-    })
+    
+    allocationService.startAllocation(allocRound)
     .then(() => {
-        console.timeEnd("start");
         successHandler(res, "Allocation completed", "Allocation succesful - Allocation");
     })
     .catch(err => {
