264241e1fe013a6b0f2d9aaf864d703328dbc230
Janita <bgj369@myy.haaga-helia.fi>
2022-11-17 11:40:17 +0200

    Api tehty jolla näkee yhden opetuksen kaikki varuste tarpeet

--- routes/subjectEquipment.js
+++ routes/subjectEquipment.js
@@ -14,11 +14,25 @@ const {
   validateAddUpdateSubjectEquipment,
 } = require("../validationHandler/index");
 
+subjectequipment.get("/getEquipment/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("id", id);
+  const sqlGetEquipmentBySubjectId =
+    "SELECT se.subjectId AS id, e.name,e.description, se.equipmentId FROM Subjectequipment se JOIN Equipment e ON se.equipmentId = e.id WHERE se.subjectid = ?;";
+  db.query(sqlGetEquipmentBySubjectId, id, (err, result) => {
+    if (err) {
+      dbErrorHandler(res, err, "Oops! Nothing came through - SubjectEquipment");
+    } else {
+      successHandler(res, result, "getEquipment successful - SubjectEquipment");
+    }
+  });
+});
+
 subjectequipment.post(
   "/post",
   validateAddUpdateSubjectEquipment,
   (req, res) => {
-   // console.log("body:", req.body);
+    // console.log("body:", req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       logger.error("Validation error:  %O", errors);
@@ -33,7 +47,8 @@ subjectequipment.post(
     const sqlInsert =
       "INSERT INTO subjectequipment (subjectId, equipmentId, priority, obligatory) VALUES (?,?,?,?)";
     db.query(
-      sqlInsert,[subjectId, equipmentId, priority, obligatory],
+      sqlInsert,
+      [subjectId, equipmentId, priority, obligatory],
       (err, result) => {
         if (!result) {
           requestErrorHandler(res, err, "Nothing to insert");
